1f62b9c9b26ae654465df0a990ea5133636eb51a
lizitang <1650731623@qq.com>
2017-06-22 17:01:15 +0800

    quick-sort

--- 十大排序算法-js/quickSort.js
+++ 十大排序算法-js/quickSort.js
@@ -5,19 +5,19 @@
 * 平均时间复杂度：O(nlogn)
 * 空间复杂度O(log n),不稳定*/
 
-    function quickSort(ele){
-        if(ele.length <= 1){
-            return ele;
+    function quickSort(elements){
+        if(elements.length <= 1){
+            return elements;
         }
-        var pivotIndex = Math.floor(ele.length / 2);//pivot中心点
-        var pivot= ele.splice(pivotIndex,1)[0];
+        var pivotIndex = Math.floor(elements.length / 2);//pivot中心点
+        var pivot= elements.splice(pivotIndex,1)[0];
         var left=[];
         var right=[];
-        for(var i=0;i<ele.length;i++){
-            if(ele[i]<pivot){
-                left.push(ele[i]);
+        for(var i=0;i<elements.length;i++){
+            if(elements[i]<pivot){
+                left.push(elements[i]);
             } else{
-                right.push(ele[i]);
+                right.push(elements[i]);
             }
         }
         return quickSort(left).concat([pivot],quickSort(right));

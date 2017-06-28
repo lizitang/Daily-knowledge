df2fc1b474e0652025f6b18f637fe9da321eb1c7
lizitang <wanyingliu@Emotibot.com>
2017-06-28 11:21:22 +0800

    解析url

---
+++ 解析url.js
@@ -0,0 +1,36 @@
+/*
+* @Author: test
+* @Date:   2017-06-26 16:13:03
+* @Last Modified by:   test
+* @Last Modified time: 2017-06-26 16:30:09
+*/
+
+'use strict';
+function parseURL(url){
+	var a=document.createElement('a');
+	a.href=url;
+	return{
+		source:url,
+		protocol:a.protocol.replace(':',''),
+		host:a.hostname,
+		port:a.port,
+		params:(function(){
+			var ret={},
+				seg=a.search.replace(/^\?/,'').split('&'),
+				len=seg.length,i=0,s;
+				for(;i<len;i++){
+					if(!seg[i]){
+						continue;
+					}
+					s=seg[i].split('=');
+					ret[s[0]] = s[1];
+				}
+				return ret;
+		}) (),
+		file : (a.pathname.math(/\/([^\/?#]+)$/i) || [,''])[1],
+		hash: a.hash.replace('#',''),
+		path:a.pathname.peplace(/^([^\/])/,'/$1'),
+		segments: a.pathname.replace(/^\//,'').split('/');
+	};
+}
+parseURL('http://www.cnblogs.com/wayou/p/');
